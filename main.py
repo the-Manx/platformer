9d0b1d4988a7a48545843600ae6822e4f73f2a6d
the-Manx <bryandgordon@hotmail.com>
2021-03-18 20:41:29 -0400

    Added Player Class and left right motion with accelleration and friction.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame as pg
 import random
 from settings import *
+from sprites import *
 
 class Game:
     def __init__(self):
@@ -17,6 +18,8 @@ class Game:
     def new(self):
         # start a new game
         self.all_sprites = pg.sprite.Group()
+        self.player = Player()
+        self.all_sprites.add(self.player)
         self.run()
 
     def run(self):
--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
 # Configuration and Settings
 
-TITLE = "My Game"
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+TITLE = "Jumpy!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
 
 #define colours
 WHITE = (255, 255, 255)
@@ -11,3 +15,4 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
