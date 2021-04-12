baa5d95b50cb6c0c635a3a9c1e4fb2baed88126f
the-Manx <bryandgordon@hotmail.com>
2021-04-12 14:45:47 -0400

    High score

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame as pg
 import random
 from settings import *
 from sprites import *
+from os import path
 
 class Game:
     def __init__(self):
@@ -15,6 +16,16 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
         self.font_name = pg.font.match_font(FONT_NAME)
+        self.load_data()
+
+    def load_data(self):
+        # load high score
+        self.dir = path.dirname(__file__)
+        try:
+            with open(path.join(self.dir, HS_FILE), 'r') as f:
+                self.highscore = int(f.read())
+        except:
+                self.highscore = 0
 
     def new(self):
         # start a new game
@@ -100,6 +111,7 @@ class Game:
         self.draw_text(TITLE, 48, WHITE, int (WIDTH / 2), int (HEIGHT / 4))
         self.draw_text("Arrows to move, Space to jump", 22, WHITE, int (WIDTH / 2), int (HEIGHT / 2))
         self.draw_text("Press a key to play", 22, WHITE, int (WIDTH /2), int (HEIGHT * 3 / 4))
+        self.draw_text("High Score: " + str(self.highscore), 22, WHITE, int (WIDTH / 2), 15)
         pg.display.flip()
         self.wait_for_key()
 
@@ -111,6 +123,13 @@ class Game:
         self.draw_text("GAME OVER", 48, WHITE, int (WIDTH / 2), int (HEIGHT / 4))
         self.draw_text("Score: " + str(self.score), 22, WHITE, int (WIDTH / 2), int (HEIGHT / 2))
         self.draw_text("Press a key to play again", 22, WHITE, int (WIDTH /2), int (HEIGHT * 3 / 4))
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.draw_text("NEW HIGH SCORE!", 22, WHITE, int (WIDTH /2), int (HEIGHT / 2) + 40)
+            with open(path.join(self.dir, HS_FILE), 'w') as f:
+                f.write(str(self.score))
+        else:
+            self.draw_text("High Score: " + str(self.highscore), 22, WHITE, int (WIDTH / 2), int (HEIGHT / 2) + 40)
         pg.display.flip()
         self.wait_for_key()
 
